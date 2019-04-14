997c052d72c6c8075e57ddb82fa5287b7e59e32c
kazuoshi <lilty722@gmail.com>
2019-04-14 15:32:48 +0900

    新しいウィンドウの修正

--- BtnEventNewWindow_sample.py
+++ BtnEventNewWindow_sample.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-def BtnEvent():
+def btn_event():
     subWindow = tk.Toplevel()
     subWindow.title("test")
     subBtn = tk.Button(subWindow, text="キャンセル", command=subWindow.destroy)
@@ -8,7 +8,7 @@ def BtnEvent():
 
 
 root = tk.Tk()
-btn1 = tk.Button(root, text="新しいウィンドウ", command=BtnEvent)
+btn1 = tk.Button(root, text="新しいウィンドウ", command=btn_event)
 btn1.pack()
 btn2 = tk.Button(root, text="閉じる", command=root.destroy)
 btn2.pack()
